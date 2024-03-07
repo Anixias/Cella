b3b03e17a4ea4ece89b60fbbe736b113a95806a7
Anixias <colnusca@gmail.com>
2024-03-07 00:47:25 -0600

    Added Diagnostic and DiagnosticList

---
+++ Cella/Diagnostics/Diagnostic.cs
@@ -0,0 +1,42 @@
+﻿using Cella.Analysis.Text;
+
+namespace Cella.Diagnostics;
+
+public enum DiagnosticSeverity
+{
+	Error,
+	Warning,
+	Hint
+}
+
+public readonly struct Diagnostic
+{
+	public readonly DiagnosticSeverity severity;
+	public readonly IBuffer source;
+	public readonly TextRange? range;
+	public readonly string message;
+	public readonly int line;
+	public readonly int column;
+
+	public Diagnostic(DiagnosticSeverity severity, IBuffer source, TextRange? range, string message)
+	{
+		this.severity = severity;
+		this.source = source;
+		this.range = range;
+		this.message = message;
+
+		if (range is null)
+		{
+			line = 0;
+			column = 0;
+			return;
+		}
+
+		(line, column) = source.GetLineColumn(range.Value.Start);
+	}
+
+	public override string ToString()
+	{
+		return message;
+	}
+}
---
+++ Cella/Diagnostics/DiagnosticList.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace Cella.Diagnostics;
+
+public sealed class DiagnosticList : IEnumerable<Diagnostic>
+{
+	public int Count => diagnostics.Count;
+
+	public DiagnosticList Errors => new(diagnostics.Where(d => d.severity == DiagnosticSeverity.Error));
+	public DiagnosticList Warnings => new(diagnostics.Where(d => d.severity == DiagnosticSeverity.Warning));
+	
+	public int ErrorCount => diagnostics.Count(d => d.severity == DiagnosticSeverity.Error);
+	public int WarningCount => diagnostics.Count(d => d.severity == DiagnosticSeverity.Warning);
+	
+	private readonly List<Diagnostic> diagnostics;
+
+	public DiagnosticList()
+	: this([])
+	{
+	}
+
+	public DiagnosticList(IEnumerable<Diagnostic> diagnostics)
+	{
+		this.diagnostics = diagnostics.ToList();
+	}
+	
+	public void Add(Diagnostic diagnostic)
+	{
+		diagnostics.Add(diagnostic);
+	}
+	
+	public void Add(IEnumerable<Diagnostic> diagnostic)
+	{
+		diagnostics.AddRange(diagnostic);
+	}
+	
+	public IEnumerator<Diagnostic> GetEnumerator()
+	{
+		return diagnostics.OrderBy(d => d.line).ThenBy(d => d.column).GetEnumerator();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator()
+	{
+		return GetEnumerator();
+	}
+}
