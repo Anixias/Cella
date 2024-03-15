e651473e5ab28a1461f058488b899c6873dd4f75
Anixias <colnusca@gmail.com>
2024-03-14 20:04:56 -0500

    Renamed console project to have no whitespace

--- src/csharp/CellaConsole/Program.cs
+++ src/csharp/CellaConsole/Program.cs
@@ -14,7 +14,7 @@ public static class Program
 		// Todo: Parse args using OptionSet
 
 		var diagnostics = new DiagnosticList();
-		var source = new StringBuffer("mod helloWorld\n\nuse std.io.Console\n\nmain: entry(args: String[]): !io Int32\n{}");
+		var source = new StringBuffer("mod helloWorld\n\nmain: entry(args: String[]): Int32\n{\n\tret 123\n}");
 		var lexer = new FilteredLexer(source);
 		var (ast, parserDiagnostics) = Parser.Parse(lexer);
 		diagnostics.Add(parserDiagnostics);
