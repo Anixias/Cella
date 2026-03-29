e9833eb56a4c30d7412a0af09d87df5f9a04ec07
Anixias <colnusca@gmail.com>
2026-03-29 09:53:06 -0500

    Fixed empty functions

--- src/csharp/src/Cella.Core/Lowering/Lowerer.cs
+++ src/csharp/src/Cella.Core/Lowering/Lowerer.cs
@@ -77,17 +77,41 @@ public sealed class Lowerer : IResolvedDeclarationNodeVisitor
 					break;
 			}
 			
-			// Collapse empty blocks
-			for (var i = function.Blocks.Count - 1; i >= 0; i--)
+			// Normalize blocks
+			if (function.Blocks.Count == 0)
 			{
-				var block = function.Blocks[i];
+				function.Blocks.Add(new BasicBlock("entry")
+				{
+					Terminator = ReturnTerminator.Void
+				});
+			}
+			else if (function.Blocks.Count == 1)
+			{
+				var block = function.Blocks[0];
 				
-				// If the block has no instructions and an undefined terminator, it wasn't actually used
-				if (block.Instructions.Count == 0 && block.Terminator == UndefinedTerminator.Instance)
-					function.Blocks.RemoveAt(i);
+				if (block.Terminator == UndefinedTerminator.Instance)
+					block.Terminator = ReturnTerminator.Void;
+			}
+			else
+			{
+				for (var i = function.Blocks.Count - 1; i >= 0; i--)
+				{
+					var block = function.Blocks[i];
+					
+					if (block.Terminator != UndefinedTerminator.Instance)
+						continue;
+					
+					// If the block has no instructions and an undefined terminator, it wasn't actually used
+					// TODO We can't just remove the block because another block could be referencing it...
+					if (block.Instructions.Count == 0)
+						function.Blocks.RemoveAt(i);
+					else
+						block.Terminator = ReturnTerminator.Void;
+				}
 			}
 			
 			// TODO Warn about unreachable code & remove
+			// TODO Add control flow/data flow analysis before codegen
 			
 			return function;
 		}
